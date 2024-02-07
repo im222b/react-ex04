ec2e98eeeaa53d04010b8bf8642544c18a514c7d
im222b <rudgnsdleh@naver.com>
2024-02-07 16:09:28 +0900

    nday2

--- src/App.tsx
+++ src/App.tsx
@@ -14,21 +14,61 @@ const Wrapper =styled.div `
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
-  background-color: white;
-  border-radius: 15px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(2555, 255, 255, 0.2);
+  border-radius: 40px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
-  start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+const Circle = styled(motion.div)`
+    box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+    background-color: white;
+    height: 70px;
+    width: 70px;
+    place-self: center;
+    border-radius: 35px;
+`;
+
+const boxVariants = {
+  start: {
+    opacity: 0,
+    scale: 0.5,
+  },
+  end: {
+    scale: 1,
+    opacity: 1,
+    transition: {
+      type: "spring",
+      duration: 0.5,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.2,
+    },
+  },
+};
+
+const cicleVariants = {
+  start : {
+    opacity: 0,
+    y: 10,
+  },
+  end : {
+    opacity: 1,
+    y: 0,
+  }
 };
 
 
 function App() {
     return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <Box variants={boxVariants} initial="start" animate="end">
+        <Circle variants={cicleVariants}/>
+        <Circle variants={cicleVariants}/>
+        <Circle variants={cicleVariants}/>
+        <Circle variants={cicleVariants}/>
+      </Box>
     </Wrapper>
     );
   }
