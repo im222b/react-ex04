968a5d0aadd301dbe0a58ef1b68c5578023b00c6
im222b <rudgnsdleh@naver.com>
2024-02-08 18:15:32 +0900

    nday3-4

--- src/App.tsx
+++ src/App.tsx
@@ -12,50 +12,74 @@ const Wrapper =styled(motion.div) `
   justify-content: center;
   align-items: center;
   background: linear-gradient(135deg,rgb(41, 213, 58),rgb(17, 174, 114));
-  
+  flex-direction: column;
 `;
 
 const Box = styled(motion.div) `
   width: 400px;
   height: 200px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  position: absolute;
   top: 100px;
+  background-color: rgba(255, 255, 255, 1);
+  position: absolute;
+  border-radius: 40px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 28px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `
-const boxVariants ={
-  initial: {
-    opacity:0,
-    scale:0
-  },
-  visible: {
-    opacity:1,
-    scale:1,
-    Z:10,
-  },
-  leaving: {
-    opacity:0,
-    scale:0,
-    y:50,
-  },
 
+const box = {
+  entry: (back:boolean) => ({
+      x:back ? -500 : 500,
+      opacity: 0,
+      scale: 0,
+  }),
+  center:{
+    x:0,
+    opacity: 1,
+    scale: 1,
+    transition: {
+      duration:1,
+    }
+  },
+  exit: (back: boolean) =>({
+    x: back ? 500: -500,
+    opacity: 0,
+    scale: 0,
+    transition: {
+      duration: 1,
+    }
+  })
 };
 
 
 function App() {
-  const [showing, setshowing] = useState(false);
-  const toggleShowing = () => setshowing((prev) => !prev);
+  const [visible, setVisible] = useState(1);
+  const [back , setBack] = useState(false);
+  const nextPlease = () => {
+    setBack(false);
+    setVisible(prev => (prev === 10 ? 10: prev + 1 ));
+  };
+  const prvePlease = () => {
+    setBack(true);
+    setVisible(prev => (prev === 1? 1: prev - 1));
+  };
     return (
     <Wrapper>
-      <button onClick={toggleShowing}>Click</button>
-        <AnimatePresence>{showing ? <Box 
-          variants={boxVariants} 
-          initial="initial" 
-          animate="visible"
-          exit="leaving"
-          /> : null}
-        </AnimatePresence>
+      <AnimatePresence custom={back}>
+
+        <Box
+          custom={back}
+          variants={box} 
+          initial="entry"
+          animate="center"
+          exit="exit"
+          key={visible}>
+        {visible} </Box> 
+      </AnimatePresence>
+      <button onClick={nextPlease}>next</button>
+      <button onClick={prvePlease}>prve</button>
     </Wrapper>
     );
   }
