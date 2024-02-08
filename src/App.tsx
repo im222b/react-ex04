e6b5f6cdbbfb47f8f2eda5bbad0350f415a1312d
im222b <rudgnsdleh@naver.com>
2024-02-08 16:17:02 +0900

    nday3-1

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,49 @@
 
 import styled from "styled-components";
-import { motion } from "framer-motion"
-import { hover } from "@testing-library/user-event/dist/hover";
-import { click } from "@testing-library/user-event/dist/click";
+import { motion, useMotionValue, useTransform, useScroll, useViewportScroll } from "framer-motion"
+import { useEffect } from "react";
 
-const Wrapper =styled.div `
+
+const Wrapper =styled(motion.div) `
   display: flex;
-  height: 100vh;
-  width: 100vh;
+  height: 500vh;
+  width: 100vw;
   display: flex;
   justify-content: center;
   align-items: center;
+  background: linear-gradient(135deg,rgb(238,0,153),rgb(221,0,238));
+  
 `;
 
 const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   display: grid;
-  background-color: rgba(2555, 255, 255, 1);
+  background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  justify-content: center;
+  align-items: center;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-
-const boxVariants = {
-  hover : {scale:1.5 ,rotateZ: 90},
-  click : {scale:1 , borderRadius: "100px"}
-};
-
-
-
 function App() {
+  const x = useMotionValue(0);
+  const rotateZ = useTransform(x, [-800, +800], [-360,  360]);
+  const gradient = useTransform(
+    x, [-800, 800], 
+    [
+      "linear-gradient(135deg,rgb(21, 169, 192),rgb(0, 87, 238))",
+      "linear-gradient(135deg,rgb(39, 221, 7),rgb(171, 203, 11))"
+    ])
+  const {scrollXProgress} = useScroll();
     return (
-    <Wrapper>
-      <Box 
-      variants={boxVariants}
-      whileHover="hover" 
-      whileTap="click" 
-      />
+    <Wrapper style={{background:gradient}}>
+        <Box
+          style={{ x , rotateZ , scale : scrollXProgress }} 
+          drag="x" 
+          dragSnapToOrigin
+
+          />
     </Wrapper>
     );
   }
--- src/index.tsx
+++ src/index.tsx
@@ -64,7 +64,7 @@ body{
   font-family: 'Dosis', sans-serif;
   color : black;
   line-height: 1.2;
-  background:linear-gradient(135deg,#e09,#d0e);
+  //background:linear-gradient(135deg,rgb(213, 6, 141),rgb(221, 0, 238));
 }
 a {
   text-decoration: none;
