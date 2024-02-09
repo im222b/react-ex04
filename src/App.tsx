5d1db77f166dd55ff1a12bff5c0caa59f9c39473
im222b <rudgnsdleh@naver.com>
2024-02-09 17:07:51 +0900

    day5

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 
 import styled from "styled-components";
-import { motion, useMotionValue, useTransform, useScroll, useViewportScroll, AnimatePresence } from "framer-motion"
-import { useEffect, useState } from "react";
-import { start } from "repl";
+import { motion, AnimatePresence } from "framer-motion"
+import {useState } from "react";
+;
 
 
 const Wrapper =styled(motion.div) `
@@ -14,32 +14,52 @@ const Wrapper =styled(motion.div) `
   background: linear-gradient(135deg,rgb(41, 213, 58),rgb(17, 174, 114));
 `;
 
+const Grid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  width: 50vw;
+  gap:10px;
+  div:first-child,
+  div:last-child {
+    grid-column:  span 2;
+  }
+`
+
 const Box = styled(motion.div) `
-  width: 400px;
-  height: 400px;
+  height: 200px;
   background-color: rgba(255, 255, 255, 1);
   border-radius: 40px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`
+const Overlay = styled(motion.div)`
+  width: 100%;
+  height: 100%;
+  position: absolute;
   display: flex;
   justify-content: center;
   align-items: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`
-const Circle = styled(motion.div)`
-  background-color: #193ffb;
-  width: 100px;
-  height: 100px;
- 
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-
 `
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked((prev) => !prev);
+  const[id,setId] = useState<null|string>(null);
+  
     return (
-      <Wrapper onClick={toggleClicked}>
-          <Box>{!clicked ? <Circle layoutId="circle" style={{borderRadius: 50}} /> : null }</Box>
-          <Box>{clicked ? <Circle layoutId="circle" style={{borderRadius: 0,scale:2} } /> : null}</Box>
+      <Wrapper>
+        <Grid>
+          {[1,2,3,4].map(n=> (
+          <Box onClick ={() => setId(n+"")} key={n} layoutId={n+""}/>))}
+        </Grid>
+        <AnimatePresence>
+          {id ? 
+          <Overlay 
+          onClick={() => setId(null)}
+          initial={{ backgroundColor : "rgba(0,0,0,0)" }} 
+          animate={{backgroundColor : "rgba(0,0,0,0.6)"}}
+          exit={{backgroundColor : "rgba(0,0,0,0)"}}
+          >
+            <Box layoutId={id} style={{height:200, width:400}}/>
+          </Overlay> : null}
+        </AnimatePresence>
       </Wrapper>
     );
   }
